1e4292d1ce90b51d0059b8ae34a3e464fad24366
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-11 18:18:46 +0200

    Tried to clean the code, added the makeformdata function but it still needs some fixing

--- js/global.js
+++ js/global.js
@@ -16,35 +16,42 @@ const ExecutePostAPI = async (api_url, api_data, api_token = null) => {
   }
 }
 
+const MakeFormData = (list_of_data) => {
+  let data = new FormData();
+  for(let i=0; i<list_of_data.length; i++){
+    console.log(Object.keys(list_of_data[i])[0]);
+    data.append(`${Object.keys(list_of_data[i])[0]}`, list_of_data[i].value);
+  }
+  console.log(data);
+  return data;
+}
 
-const PostRegistrationData = async (register_patient_url) => {
-  let name = document.getElementById("Name").value;
-  let date_of_birth = document.getElementById("Date-of-birth").value;
-  let email = document.getElementById("Email").value;
-  let password = document.getElementById("Password").value;
-  let verify_password = document.getElementById("Verify-password").value;
+
+const PostRegistrationData = async (url) => {
+  let name = document.getElementById("Name");
+  let dob = document.getElementById("Date-of-birth");
+  let email = document.getElementById("Email");
+  let password = document.getElementById("Password");
+  let verify_password = document.getElementById("Verify-password");
+  
+  let list_of_data = [];
+  list_of_data.push(name,dob,email,password,verify_password);
+  console.log(list_of_data);
 
   if(password == verify_password){
-    let data = new FormData();
-
-    data.append("name", name);
-    data.append("password", password);
-    data.append("email", email);
-    data.append("dob", date_of_birth);
-    const response = await ExecutePostAPI(register_patient_url, data);
+    let data =  MakeFormData(list_of_data);
+    const response = await ExecutePostAPI(url, data);
     console.log(response);
 
   }else{
     console.log("Not the same password");
   }
-
 }
 
 //Page Functions
 
 const LoadRegistration = async () => {
   const register_patient_url = baseurl + "Register.php";
-
   const register_button = document.getElementById("Signup-button");
   register_button.addEventListener("click", () => PostRegistrationData(register_patient_url));
 }
