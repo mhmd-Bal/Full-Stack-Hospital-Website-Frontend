024dee55053b019fdfcea969112df61e2445bc9f
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-11 17:07:30 +0200

    Added the get api function

--- js/global.js
+++ js/global.js
@@ -1,2 +1,17 @@
 const baseurl = "http://localhost/Full-Stack-Hopital-Website\Full-Stack-Hospital-Website-Backend/";
 
+const ExecuteGetAPI = async (api_url) => {
+  try{
+    return await axios(api_url);
+  }catch(error){
+    console.log(error);
+  }
+}
+
+const ExecutePostAPI = async (api_url, api_data, api_token = null) => {
+  try{
+    return await axios(api_url, api_data, api_token = null);
+  }catch(error){
+    console.log(error);
+  }
+}
